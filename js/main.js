b00c3373623ccba04e5b9235a9c0a3901aca4afb
A-Wata5555 <141040252+A-Wata5555@users.noreply.github.com>
2023-07-31 23:04:26 +0900

    最終課題

---
+++ js/main.js
@@ -0,0 +1,52 @@
+function Display(){
+    let now = new Date();
+    var YYYY = now.getFullYear();
+    var MM = now.getMonth()+1;
+    var DD = now.getDate();
+    alert(YYYY+"/"+MM+"/"+DD);
+}
+
+function Jan_3(hidarite){
+    //グーが0
+    //チョキが1
+    //パーが2
+    let migite = Math.floor(Math.random()*3);
+    let img = document.getElementById("kekka");
+    let katimake = document.getElementById("resule");
+
+    if(hidarite === 0){
+        if(migite === 0){
+            img.src = "./gallery/Jan_gu.jpg";
+            katimake.innerHTML = "あいこ";
+        }else if(migite === 1){
+            img.src = "./gallery/Jan_tyo.jpg";
+            katimake.innerHTML = "勝ち";
+        }else if(migite === 2){
+            img.src = "./gallery/Jan_par.jpg";
+            katimake.innerHTML = "負け";
+        }
+    }else if(hidarite === 1){
+        if(migite === 0){
+            img.src = "./gallery/Jan_gu.jpg";
+            katimake.innerHTML = "負け";
+        }else if(migite === 1){
+            img.src = "./gallery/Jan_tyo.jpg";
+            katimake.innerHTML = "あいこ";
+        }else if(migite === 2){
+            img.src = "./gallery/Jan_par.jpg";
+            katimake.innerHTML = "勝ち";
+        }
+    }else if(hidarite === 2){
+        if(migite === 0){
+            img.src = "./gallery/Jan_gu.jpg";
+            katimake.innerHTML = "勝ち";
+        }else if(migite === 1){
+            img.src = "./gallery/Jan_tyo.jpg";
+            katimake.innerHTML = "負け";
+        }else if(migite === 2){
+            img.src = "./gallery/Jan_par.jpg";
+            katimake.innerHTML = "あいこ";
+        }
+    }
+
+}
